330d5ef91324ddcd2d5f94feca2421df776a3f29
gae-bak <gs04198@naver.com>
2021-08-08 20:45:03 +0900

    Initial commit

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,52 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String hellomvc(@RequestParam(value ="name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public  String helloString(@RequestParam("name") String name) {
+        return "hello" + name; // hello "name" view 없이 그대로 나옴
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody // json을 기본으로 반환하자.
+    public Hello helloApi(@RequestParam("name") String name) {
+         Hello hello = new Hello();
+         hello.setName(name);
+         return hello;
+    }
+
+    static class Hello { // ctrl + n = getter setter 열기
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+}
+//spirng-boot-devtools -> 서버 재시작없이 view 파일 변경
