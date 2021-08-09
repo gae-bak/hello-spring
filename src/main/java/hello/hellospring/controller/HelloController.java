f289b7e82a3bd04c890f9398e33e1ed805364d0f
gae-bak <sb04198@naver.com>
2021-08-10 00:07:46 +0900

    깃 잔디 오류 테스트

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
-
+//깃 잔디 오류..
 
     @GetMapping("hello")
     public String hello(Model model) {
