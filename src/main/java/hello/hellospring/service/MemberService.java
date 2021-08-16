c400c5b17bb609816d26b10a4596f07f6f91af8f
gae-bak <sb04198@naver.com>
2021-08-16 23:09:09 +0900

    2021 08 16 자바 객체지향 보충 및 강의 시청

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -4,6 +4,7 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
@@ -14,14 +15,28 @@ public class MemberService {
     회원 가입
     */
      public Long join(Member member){
-         Optional<Member> result = memberRepository.findByName(member.getName());
-         result.ifPresent(member1 -> {
-             throw new IllegalStateException("이미 존재하는 회원입니다.");
-         });
+         // ifPresent -> ~가 있으면
+         validateDuplicateMember(member); // 중복 회원 검증
 
          memberRepository.save(member);
          return  member.getId();
      }
 
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+               .ifPresent(member1 -> {
+                   throw new IllegalStateException("이미 존재하는 회원입니다.");
+               });
+    }
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+       return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
 
 }
